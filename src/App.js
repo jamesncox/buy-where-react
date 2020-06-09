51f219e57917ed68408e16319872d1d6d40b2d04
jamesncox <jamesncox@yahoo.com>
2020-06-09 16:14:27 -0500

    Create Login.js and login form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import './stylesheets/App.css';
 import { connect } from 'react-redux';
 import { getToken } from './actions/sessions'
+import Login from './components/User/Login'
 
 function App(props) {
   useEffect(() => {
@@ -12,6 +13,7 @@ function App(props) {
     <div className="App">
       <header className="App-header">
         <h1>Store Spender</h1>
+        <Login />
       </header>
     </div>
   );

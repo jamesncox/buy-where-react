eeae58eb82cc4b316e46d38b663a0f3443c05202
jamesncox <jamesncox@yahoo.com>
2020-06-09 16:42:39 -0500

    Change App.js to class component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { Component } from 'react';
 import './stylesheets/App.css';
 import { connect } from 'react-redux';
 import { getToken } from './actions/sessions'
 import Login from './components/User/Login'
 import Signup from './components/User/Signup'
 
-function App(props) {
-  useEffect(() => {
-    getToken()
-  }, [])
+class App extends Component {
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Store Spender</h1>
-        <Signup />
-        <Login />
-      </header>
-    </div>
-  );
+  componentDidMount() {
+    this.props.getToken()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Store Spender</h1>
+          <Signup />
+          <Login />
+        </header>
+      </div>
+    )
+  }
 }
 
-export default connect(null, getToken)(App);
+export default connect(null, { getToken })(App);

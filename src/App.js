a9fa87d5baa24e06a7d086e6b1a5c6655b33178d
jamesncox <jamesncox@yahoo.com>
2020-06-10 11:56:10 -0500

    Add Router/switch to App.js / Add Home.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 import './stylesheets/App.css';
 import { connect } from 'react-redux';
 import { getToken } from './actions/sessions'
 
 import Header from './components/Layout/Header'
+import Home from './components/Layout/Home'
 import Login from './components/User/Login'
 import Signup from './components/User/Signup'
 
@@ -15,13 +21,22 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-
-        <Header />
-        <Signup />
-        <Login />
-
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/signup">
+              <Signup />
+            </Route>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     )
   }
 }
---
+++ src/components/Layout/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home(props) {
+    return (
+        <p>
+            This is the home page
+        </p>
+    )
+}
+
+export default Home

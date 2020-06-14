308499c0b26129170082c1aaea22d600ce8e8455
jamesncox <jamesncox@yahoo.com>
2020-06-14 09:50:08 -0500

    Add link to home/profile page to SignInSuccess

--- src/components/User/SignInSuccess.js
+++ src/components/User/SignInSuccess.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import { makeStyles } from '@material-ui/core/styles';
+import { Link as RouterLink } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -10,7 +11,6 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
 import { useWideCardMediaStyles } from '@mui-treasury/styles/cardMedia/wide';
 
 const useStyles = makeStyles((theme) => ({
@@ -50,7 +50,11 @@ function SignInSuccess(props) {
                         </CardContent>
                     </CardActionArea>
                     <CardActions>
-                        <Button size="small" color="primary">
+                        <Button
+                            size="small"
+                            color="primary"
+                            component={RouterLink}
+                            to="/">
                             Profile
                         </Button>
                         <Button size="small" color="primary">

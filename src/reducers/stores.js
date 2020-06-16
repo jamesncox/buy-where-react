7b5a680ceaa122d059b9b60d43c56b0411df8673
jamesncox <jamesncox@yahoo.com>
2020-06-16 11:04:21 -0500

    Add actions to reducers/stores

--- src/reducers/stores.js
+++ src/reducers/stores.js
@@ -1,17 +1,29 @@
 import {
     SET_STORES,
-    LOADING_STORES
+    LOADING_STORES,
+    ADD_STORE,
+    STORE_ERRORS
 } from '../actionTypes'
 
-export default (state = { stores: [], loading: true }, action) => {
+export default (state = {
+    stores: [],
+    loading: true,
+    newStore: {},
+    errors: null
+}, action) => {
     switch (action.type) {
 
         case LOADING_STORES:
             return { ...state, stores: [...state.stores], loading: true }
 
         case SET_STORES:
-            console.log(action.payload)
             return { ...state, stores: action.payload, loading: false }
+
+        case ADD_STORE:
+            return { ...state, newStore: action.payload }
+
+        case STORE_ERRORS:
+            return { ...state, errors: action.payload }
 
         default:
             return state

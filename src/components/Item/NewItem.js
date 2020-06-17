9ec1243fde289b20f9cfec24871840a20ce22948
jamesncox <jamesncox@yahoo.com>
2020-06-17 11:37:54 -0500

    Debug grid input for NewItem

--- src/components/Item/NewItem.js
+++ src/components/Item/NewItem.js
@@ -2,7 +2,32 @@ import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import { createItem } from '../../actions/items'
 
+import Grid from '@material-ui/core/Grid'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button';
+import { Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(1),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+    form: {
+        margin: 'auto',
+        width: '90%',
+    },
+    submit: {
+        margin: theme.spacing(5, 0, 2),
+    },
+}))
+
 function NewItem(props) {
+    const classes = useStyles()
 
     const [name, setName] = useState("")
     const [price, setPrice] = useState(null)
@@ -37,9 +62,71 @@ function NewItem(props) {
     }
 
     return (
-        <div>
-            <p> new item form </p>
-        </div>
+        <Grid className={classes.root}>
+            <Typography className={classes.paper}>
+                Create Item
+            </Typography>
+            <form
+                className={classes.form}
+                noValidate
+                onSubmit={e => handleSubmit(e)}
+            >
+                <Grid item xs={12}>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="name"
+                        label="Item Name"
+                        name="name"
+                        autoComplete="name"
+                        onChange={handleName}
+                        value={name}
+                        autoFocus
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="quantity"
+                        label="Item quantity"
+                        name="quantity"
+                        autoComplete="quantity"
+                        onChange={handleQuantity}
+                        value={quantity}
+                        autoFocus
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="price"
+                        label="Item Price"
+                        name="price"
+                        autoComplete="price"
+                        onChange={handlePrice}
+                        value={price}
+                        autoFocus
+                    />
+                </Grid>
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}
+                >
+                    Create Item
+                </Button>
+            </form>
+        </Grid>
     )
 }
 
@@ -47,4 +134,4 @@ const mapStateToProps = state => ({
     store: state.stores.store
 })
 
-export default connect(mapStateToProps, { crateItem })(NewItem)
+export default connect(mapStateToProps, { createItem })(NewItem)

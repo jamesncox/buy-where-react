0683bc44e71027ffe270b588db3093fc4750d205
jamesncox <jamesncox@yahoo.com>
2020-06-17 10:48:27 -0500

    Update NewItem with useState methods

--- src/components/Item/NewItem.js
+++ src/components/Item/NewItem.js
@@ -1,7 +1,40 @@
 import React, { useState } from 'react'
 import { connect } from 'react-redux'
+import { createItem } from '../../actions/items'
 
 function NewItem(props) {
+
+    const [name, setName] = useState("")
+    const [price, setPrice] = useState(null)
+    const [quantity, setQuantity] = useState(null)
+
+    const handleName = (e) => {
+        setName(e.target.value)
+    }
+
+    const handlePrice = (e) => {
+        setPrice(e.target.value)
+    }
+
+    const handleQuantity = (e) => {
+        setQuantity(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        const item = {
+            name: name,
+            price: price,
+            quantity: quantity,
+            storeId: props.store.id
+        }
+
+        props.createItem(item)
+        setName("")
+        setPrice(null)
+        setQuantity(null)
+    }
 
     return (
         <div>
@@ -10,4 +43,8 @@ function NewItem(props) {
     )
 }
 
-export default NewItem
+const mapStateToProps = state => ({
+    store: state.stores.store
+})
+
+export default connect(mapStateToProps, { crateItem })(NewItem)

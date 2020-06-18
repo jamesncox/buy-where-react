1b0544d0bdfd59b9c826525fe8c1386c02901f94
jamesncox <jamesncox@yahoo.com>
2020-06-17 22:48:15 -0500

    Debug ADD_ITEM_TO_STORE reducer / not working

--- src/reducers/stores.js
+++ src/reducers/stores.js
@@ -25,9 +25,22 @@ export default (state = {
             return { ...state, stores: [...state.stores, action.payload] }
 
         case ADD_ITEM_TO_STORE:
-            let specificStore = state.stores.filter(store => store.id === state.storeId)
+            // let specificStore = state.stores.filter(store => store.id === state.storeId)
             debugger
-            return { ...state, stores: [...state.stores] }
+            const itemToAdd = action.payload
+            const index = state.stores.findIndex(
+                store => store.id === state.storeId
+            )
+            const storeToUpdate = {
+                ...state.stores.slice(index, index + 1)[0]
+            }
+            const updatedItemsArr = [
+                ...storeToUpdate.items, itemToAdd
+            ]
+            const updatedStore = {
+                ...storeToUpdate, items: updatedItemsArr
+            }
+            return { ...state.stores.slice(0, index), updatedStore, ...state.stores.slice(index + 1) }
 
         case STORE_ERRORS:
             return { ...state, errors: action.payload }

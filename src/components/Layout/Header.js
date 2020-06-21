5fb82215114561e94f9c1d1950b67fc8b64821fc
jamesncox <jamesncox@yahoo.com>
2020-06-21 14:53:01 -0500

    Remove comments from Header.js

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -53,11 +53,8 @@ const useStyles = makeStyles((theme) => ({
 function ScrollTop(props) {
     const { children } = props;
     const classes = useStyles();
-    // Note that you normally won't need to set the window ref as useScrollTrigger
-    // will default to window.
-    // This is only being set here because the demo is in an iframe.
+
     const trigger = useScrollTrigger({
-        // target: window ? window() : undefined,
         disableHysteresis: true,
         threshold: 100,
     });
@@ -81,10 +78,6 @@ function ScrollTop(props) {
 
 ScrollTop.propTypes = {
     children: PropTypes.element.isRequired,
-    /**
-     * Injected by the documentation to work in an iframe.
-     * You won't need it on your project.
-     */
     window: PropTypes.func,
 };
 

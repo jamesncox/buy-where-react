1d93d6722f1f6aad1ac4d2c96d0fb41745071ffb
jamesncox <jamesncox@yahoo.com>
2020-06-22 21:12:03 -0500

    Add new/edit store/item close actions

--- src/reducers/isOpen.js
+++ src/reducers/isOpen.js
@@ -1,7 +1,10 @@
 import {
     NEW_ITEM_OPEN,
+    NEW_ITEM_CLOSE,
     NEW_STORE_OPEN,
-    EDIT_STORE_OPEN
+    NEW_STORE_CLOSE,
+    EDIT_STORE_OPEN,
+    EDIT_STORE_CLOSE
 } from '../actionTypes'
 
 export default (state = {
@@ -13,11 +16,20 @@ export default (state = {
         case NEW_ITEM_OPEN:
             return { newItemOpen: true }
 
+        case NEW_ITEM_CLOSE:
+            return { newItemOpen: false }
+
         case NEW_STORE_OPEN:
             return { newStoreOpen: true }
 
+        case NEW_STORE_CLOSE:
+            return { newStoreOpen: false }
+
         case EDIT_STORE_OPEN:
             return { editStoreOpen: true }
+
+        case EDIT_STORE_CLOSE:
+            return { editStoreOpen: false }
 
         default:
             return state

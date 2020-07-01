29e0351a0a238b9a82e82be5f1d575b8cd6a8ca8
jamesncox <jamesncox@yahoo.com>
2020-07-01 11:57:47 -0500

    Debug PieChart show store/items

--- src/components/Stats/PieChart.js
+++ src/components/Stats/PieChart.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import * as React from 'react';
 import { connect } from 'react-redux'
 import NoStatsYet from '../Layout/NoStatsYet'
 import Paper from '@material-ui/core/Paper';
@@ -10,79 +10,51 @@ import {
 
 import { Animation } from '@devexpress/dx-react-chart';
 
-// const data = [
-//     { country: 'Russia', area: 12 },
-//     { country: 'Canada', area: 7 },
-//     { country: 'USA', area: 7 },
-//     { country: 'China', area: 7 },
-//     { country: 'Brazil', area: 6 },
-//     { country: 'Australia', area: 5 },
-//     { country: 'India', area: 2 },
-//     { country: 'Others', area: 55 },
-// ];
+class PieChart extends React.PureComponent {
 
-
-function PieChart(props) {
-    const [data, setData] = useState([])
-
-
-    // const { data: chartData } = this.state;
-
-    // return (
-    //     <Paper>
-    //         <Chart
-    //             data={chartData}
-    //         >
-    //             <PieSeries
-    //                 valueField="area"
-    //                 argumentField="country"
-    //             />
-    //             <Title
-    //                 text="Total Spending At Stores"
-    //             />
-    //             <Animation />
-    //         </Chart>
-    //     </Paper>
-    // );
-
-    const renderPieChart = (id) => {
-        const userStores = props.stores.filter(store => store.user_id === id)
+    renderPieChart = (id) => {
+        const userStores = this.props.stores.filter(store => store.user_id === this.props.user.id).reverse()
         return (
             userStores.map(store => {
-                const userItems = props.items.filter(item => item.store_id === store.id)
+                const userItems = this.props.items.filter(item => item.store_id === store.id)
                 return (
-                    < Paper >
-                        <Chart
-                            data={store}
-                        >
-                            {userItems.map((item) => (
-                                <PieSeries
-                                    valueField={item.price}
-                                    argumentField={item.name}
-                                />
-                            ))}
-                            <Title
-                                text={store.name}
-                            />
-                            <Animation />
-                        </Chart>
-                    </Paper >
+                    userItems.map(item => {
+                        return (
+                            <Paper>
+                                <Chart
+                                    data={userItems}
+                                >
+                                    <PieSeries
+                                        valueField={item.price}
+                                        argumentField={item.name}
+                                    />
+                                    <Title
+                                        text={store.name}
+                                    />
+                                    <Animation />
+                                </Chart>
+                            </Paper>
+                        )
+                    })
                 )
             })
-        );
+        )
     }
 
-    const hasStores = props.stores.filter(store => store.user_id === props.user.id)
-    if (hasStores.length === 0) {
-        return <NoStatsYet />
-    } else {
-        return renderPieChart(props.user.id)
+    render() {
+        const hasStores = this.props.stores.filter(store => store.user_id === this.props.user.id)
+        if (hasStores.length === 0) {
+            return <NoStatsYet />
+        } else {
+            return this.renderPieChart(this.props.user.id)
+        }
     }
 }
 
 const mapStateToProps = state => ({
     user: state.users.user,
-    stores: state.stores.stores
+    stores: state.stores.stores,
+    items: state.items.items
 })
 
 export default connect(mapStateToProps)(PieChart)
